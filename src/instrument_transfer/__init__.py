693d7dbe048eff8b164405df10d9faf1214f943e
Sergei V. Kalinin <54861187+SergeiVKalinin@users.noreply.github.com>
2026-05-02 16:20:38 -0400

    Update __init__.py

--- src/instrument_transfer/__init__.py
+++ src/instrument_transfer/__init__.py
@@ -1,1 +1,36 @@
+from .trajectories import (
+    generate_scan_path,
+    make_rectangular_raster,
+    make_spiral,
+    make_lissajous,
+)
 
+from .instruments import (
+    spm_transfer_function,
+    build_spm_params_hysteresis_dominated,
+)
+
+from .metrics import (
+    evaluate_smooth_coverage,
+    evaluate_smooth_distortion,
+    evaluate_path_quality,
+)
+
+from .imaging import (
+    make_checkerboard,
+    simulate_unknown_trajectory_image,
+)
+
+__all__ = [
+    "generate_scan_path",
+    "make_rectangular_raster",
+    "make_spiral",
+    "make_lissajous",
+    "spm_transfer_function",
+    "build_spm_params_hysteresis_dominated",
+    "evaluate_smooth_coverage",
+    "evaluate_smooth_distortion",
+    "evaluate_path_quality",
+    "make_checkerboard",
+    "simulate_unknown_trajectory_image",
+]
